81829679aaaa660382b9b601e39aa3a031f30637
jmonsalvo <jmonsa13@gmail.com>
2022-09-08 15:50:23 -0500

    Bug fixed analisis Automatico celula1

--- 02_Auto_run/Autorun_Analisis.bat
+++ 02_Auto_run/Autorun_Analisis.bat
@@ -1,4 +1,3 @@
 set env_path=E:\ENV\
 call %env_path%\Robot_Esmaltado\Scripts\activate.bat
 call python "E:\OneDrive - Corona\03_Proy_ID\03_MACHINE_LEARNING\01_Projects\08_Proyecto_Trazabilidad\01_Robot_Esmaltado\01_Online\02_Auto_run\Main_Daily_Analisis.py"
-pause
